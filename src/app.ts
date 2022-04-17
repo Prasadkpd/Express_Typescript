e399db733364036d871849c01f53961d541c9007
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-04-17 12:09:11 +0530

    done

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.send("Hello World");
+app.get('/', (req: Request, res: Response) => {
+    return res.send("Hello World");
 });
 
-app.listen(3000, () => {
-    console.log("Application listen at http://localhost:3000");
+app.post("/api/data", (req: Request, res: Response) => {
+    console.log(req.body);
+    return res.sendStatus(200);
+});
+
+app.all("/api/all", (req: Request, res: Response) => {
+    return res.sendStatus(200);
+})
+
+app.listen(4000, () => {
+    console.log("Application listen at http://localhost:4000");
 });
