fd516ce6860060786d9317419445eb8e40b1a4d3
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-04-17 18:38:15 +0530

    security

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,14 @@
-import express, {NextFunction, Request, Response} from 'express';
-import routes from "./routes";
+import express, {Request, Response} from 'express';
+import helmet from "helmet";
 
 const app = express();
+
 app.use(express.json());
 
-//Customizing the middleware with curring
-const middleware = ({name}: { name: String }) =>
-    (req: Request, res: Response, next: NextFunction) => {
-        //@ts-ignore
-        req.name = name;
-        next();
-    }
-//Apply this middleware to every route
-app.use(middleware({name: "Prasad"}));
+app.use(helmet);
 
-routes(app);
+app.get("/", (req:Request, res: Response))
+
 app.listen(3000, () => {
     console.log("Application listen at http://localhost:3000");
 });
