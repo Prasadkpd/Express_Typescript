62692dd939823ce40325a69cd72cf5d18baf2cbe
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-04-17 14:44:43 +0530

    done

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express, {Request, Response} from 'express';
 const app = express();
 app.use(express.json());
 
-app.get("/health", (req: Request, res: Response) => res.sendStatus(200));
-app.get("/ab*cd", (req: Request, res: Response) => res.send("/ab*cd"));
-app.get(/abc/, (req: Request, res: Response) => res.send("abc"));
+app.get("/api/books/:bookId/:authorId", (req: Request, res: Response) => {
+    console.log(req.params.bookId);
+    console.log(req.params.authorId);
+    return res.send(req.params);
+});
 
 app.listen(3000, () => {
     console.log("Application listen at http://localhost:3000");
