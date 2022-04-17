66c241ceba8b56198a0621977ac61431a3932116
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-04-17 15:23:59 +0530

    middle ware

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,22 @@ import express, {NextFunction, Request, Response} from 'express';
 
 const app = express();
 app.use(express.json());
-//First way with two functions
-// const handleGetBookOne = (req:Request, res:Response, next:NextFunction) => {
-//   console.log(req.params);
-//   next();
-// }
-//
-// const handleGetBookTwo = (req:Request, res:Response, next:NextFunction) => {
-//   console.log("Second handler");
-//   return res.send(req.params);
-// }
 
-// app.get("/api/books/:bookId/:authorId", [handleGetBookOne, handleGetBookTwo]);
-//second way
-app.get("/api/books/:bookId/:authorId", function (req: Request, res: Response, next: NextFunction) {
-    console.log(req.params);
+const middleware = (req: Request, res: Response, next: NextFunction) => {
+    //@ts-ignore
+    req.name = "Prasad"
     next();
-}, function (req: Request, res: Response, next: NextFunction) {
-    return res.send(req.params);
-});
+}
+
+app.get("/api/books/:bookId/:authorId",
+    middleware,
+    (req: Request, res: Response, next: NextFunction) => {
+        //@ts-ignore
+        console.log(req.name);
+        //@ts-ignore
+        res.send(req.name);
+    }
+);
 
 app.listen(3000, () => {
     console.log("Application listen at http://localhost:3000");
