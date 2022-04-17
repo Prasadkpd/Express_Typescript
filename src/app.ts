e1291dff7aa56721ecad821ef0b05b772f7758d0
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-04-17 14:54:20 +0530

    with two functions

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,17 @@ import express, {NextFunction, Request, Response} from 'express';
 const app = express();
 app.use(express.json());
 
-const handleGetBook = (req:Request, res:Response, next:NextFunction) => {
+const handleGetBookOne = (req:Request, res:Response, next:NextFunction) => {
   console.log(req.params);
+  next();
+}
+
+const handleGetBookTwo = (req:Request, res:Response, next:NextFunction) => {
+  console.log("Second handler");
   return res.send(req.params);
 }
 
-app.get("/api/books/:bookId/:authorId", handleGetBook);
+app.get("/api/books/:bookId/:authorId", [handleGetBookOne, handleGetBookTwo]);
 
 app.listen(3000, () => {
     console.log("Application listen at http://localhost:3000");
