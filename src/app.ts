d54e98c42b4d85766307dd105ac893ae4a00f701
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-04-17 14:58:13 +0530

    with two functions

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,25 @@ import express, {NextFunction, Request, Response} from 'express';
 
 const app = express();
 app.use(express.json());
+//First way with two functions
+// const handleGetBookOne = (req:Request, res:Response, next:NextFunction) => {
+//   console.log(req.params);
+//   next();
+// }
+//
+// const handleGetBookTwo = (req:Request, res:Response, next:NextFunction) => {
+//   console.log("Second handler");
+//   return res.send(req.params);
+// }
 
-const handleGetBookOne = (req:Request, res:Response, next:NextFunction) => {
-  console.log(req.params);
-  next();
-}
-
-const handleGetBookTwo = (req:Request, res:Response, next:NextFunction) => {
-  console.log("Second handler");
-  return res.send(req.params);
-}
-
-app.get("/api/books/:bookId/:authorId", [handleGetBookOne, handleGetBookTwo]);
+// app.get("/api/books/:bookId/:authorId", [handleGetBookOne, handleGetBookTwo]);
+//second way
+app.get("/api/books/:bookId/:authorId", function (req: Request, res: Response, next: NextFunction) {
+    console.log(req.params);
+    next();
+}, function (req: Request, res: Response, next: NextFunction) {
+    return res.send(req.params);
+});
 
 app.listen(3000, () => {
     console.log("Application listen at http://localhost:3000");
